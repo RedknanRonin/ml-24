3a7ee7d1beab3862efe45601ca47a13183c13e9b
aleks_3vs4n4y <aleksi.alatalo@gmail.com>
2024-10-08 16:38:10 +0300

    update variables

--- main.py
+++ main.py
@@ -21,21 +21,20 @@ df["Composer"] = df["Composer"].apply(lambda x: 1 if x=="Yes" else 0)
 df["Foreign languages"] = df["Foreign languages"].apply(lambda x: 1 if x=="Yes" else 0)
 df["Exploratory"] = df["Exploratory"].apply(lambda x: 1 if x=="Yes" else 0)
 df["Mental Health"] = (df["Anxiety"] + df["Depression"] + df["Insomnia"] + df["OCD"])/4
+df.drop(["Anxiety", "Depression", "Insomnia", "OCD"], axis=1, inplace=True)
 df = df.dropna()
 df= df.select_dtypes(include=['number'])
 data= df
+print (data.columns)
 
 def modelByLinearRegression():
 
-    encoded = pd.get_dummies(df, columns = ['Fav genre'], dtype=int)
-    encoded = encoded.select_dtypes(include=['number'])
-    encoded.info()
-
+    encoded = df.select_dtypes(include=['number'])
 
     a = encoded[['Age','Hours per day', 'While working', 'Instrumentalist', 'Composer', 'Exploratory', 'Foreign languages', 'BPM', 'Fav genre_Classical', 'Fav genre_Country', 'Fav genre_EDM', 'Fav genre_Folk',
                  'Fav genre_Gospel', 'Fav genre_Hip hop', 'Fav genre_Jazz', 'Fav genre_K pop', 'Fav genre_Latin', 'Fav genre_Lofi', 'Fav genre_Metal', 'Fav genre_Pop', 'Fav genre_R&B', 'Fav genre_Rap',
                  'Fav genre_Rock', 'Fav genre_Video game music', 'Mental Health']]
-    print(a)
+    #print(a)
 
     ct = ColumnTransformer([
         ('somename', StandardScaler(), ['Age','Hours per day', 'While working', 'Instrumentalist', 'Composer', 'Exploratory', 'Foreign languages', 'BPM', 'Fav genre_Classical', 'Fav genre_Country', 'Fav genre_EDM', 'Fav genre_Folk',
@@ -45,12 +44,12 @@ def modelByLinearRegression():
     b = ct.fit_transform(a[['Age','Hours per day', 'While working', 'Instrumentalist', 'Composer', 'Exploratory', 'Foreign languages', 'BPM', 'Fav genre_Classical', 'Fav genre_Country', 'Fav genre_EDM', 'Fav genre_Folk',
                             'Fav genre_Gospel', 'Fav genre_Hip hop', 'Fav genre_Jazz', 'Fav genre_K pop', 'Fav genre_Latin', 'Fav genre_Lofi', 'Fav genre_Metal', 'Fav genre_Pop', 'Fav genre_R&B', 'Fav genre_Rap',
                             'Fav genre_Rock', 'Fav genre_Video game music', 'Mental Health']])
-    print(b)
+    #print(b)
 
     X = b[:, :24]
     y = b[:, -1]
-    print(X)
-    print(y)
+    #print(X)
+    #print(y)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 20, test_size = 0.2)
     y_test.shape
@@ -59,6 +58,8 @@ def modelByLinearRegression():
 
     mae=mean_absolute_error(y_pred, y_test)
     mse=mean_squared_error(y_test, y_pred)
+    print("Mean Absolute Error for linear regression: ", mae)
+    print("Mean Squared Error for linear regression: ", mse)
 
     plt.scatter(y_test, y_pred)
     plt.xlabel('Actual')
@@ -68,28 +69,28 @@ def modelByLinearRegression():
 
 
 def modelByLassoRegression():
-    X = data.drop("Mental Health", axis=1)
+    #X contain 'Age','Hours per day', 'While working', and fav genre columns
+    X = data.drop(["Mental Health","Instrumentalist",'Composer', 'Exploratory', 'Foreign languages', 'BPM'], axis=1)
     y = data["Mental Health"]
+    #print(y.sum()/len(y))
+
 
     (X_train,X_test,y_train,y_test)=train_test_split(X,y)
-    model= Lasso().fit(X_train,y_train)
+    model= Lasso(0.1).fit(X_train,y_train)
     y_pred=model.predict(X_test)   # predicting values for
 
     mse = mean_squared_error(y_test, y_pred)
     print("Mean Squared Error for Lasso regression: ", mse)
-    print(X_test)
-    print(y_test)
-    print(y_pred.shape)
 
     plt.scatter(y_test, y_pred, color="b")
     plt.xlabel('Actual mental health score')    # set the label for the x/y-axis
     plt.ylabel('Predicted mental health score')
-    #plt.plot( y_test,y_pred, color='r', label='h(x)')
 
-
-    plt.legend(loc='best',fontsize=14)
+    plt.title("Actual mental health score in relation to predicted score",fontsize=14)
 
     plt.show()  # display the plot on the screen
 
-
+modelByLinearRegression()
 modelByLassoRegression()
+
+
